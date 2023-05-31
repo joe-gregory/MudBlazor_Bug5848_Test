bccaec8a3b3ac06524c595e00952c604c102a547
joe-gregory <josephedward2nd@gmail.com>
2023-05-30 17:14:27 -0700

    imported local MudBlazor package. Trying to replicate bug

--- MudBlazor_Bug5848_Test/Client/Program.cs
+++ MudBlazor_Bug5848_Test/Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor_Bug5848_Test;
+using MudBlazor.Services;
 
 namespace MudBlazor_Bug5848_Test
 {
@@ -13,6 +14,8 @@ namespace MudBlazor_Bug5848_Test
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
+
 
             await builder.Build().RunAsync();
         }
